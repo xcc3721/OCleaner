d032f979c51bfd7cc695c39aad157f90ed92fe3c
弃凡 <chenchen.xcc@alibaba-inc.com>
2014-08-26 15:53:03 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+#coding=utf-8
+import sys, getopt
+import os
+import pathlib
+
+supportedFileType = {
+}
+
+tobeAnalizedFileType = [
+    ".png", ".jpg"
+]
+
+tobeAnalizedFileDictionary = {
+}
+
+def recursiveDirFileList(path):
+    list = [x for x in path.iterdir()]
+    dirList = []
+    fileList = []
+    for val in list:
+        if val.is_dir():
+            dirList.append(val)
+        elif val.is_file():
+            fileList.append(val)
+    for val in dirList:
+        subFileList = recursiveDirFileList(val)
+        if not subFileList is None:
+            fileList += subFileList
+    return fileList
+
+def sourceFileAnalizer(file):
+
+    return
+
+def xibFileAnalizer(file):
+    # print(path)
+    return
+
+def setupSupportedFile():
+    supportedFileType[".m"] = sourceFileAnalizer
+    supportedFileType[".xib"] = xibFileAnalizer
+    return
+
+def main(argv):
+    workingDir = pathlib.Path.cwd()
+    try:
+        opts, args = getopt.getopt(argv, None)
+        if len(args) > 0:
+            workingDir = args[0]
+            pass
+        pass
+    except getopt.GetoptError:
+        print("Error")
+        raise e
+    path = pathlib.Path(os.path.expanduser(workingDir))
+    if not path.exists():
+        print("Illegel working path %s" % path)
+        return
+    setupSupportedFile()
+    files = recursiveDirFileList(path)
+    for filepath in files:
+        if len(filepath.suffixes) > 0:
+            suffix = filepath.suffix
+            if suffix in tobeAnalizedFileType:
+                tobeAnalizedFileDictionary[filepath.name] = 0
+                pass
+
+            if suffix in supportedFileType:
+                f = open(filepath.as_posix(), 'r')
+                file = f.read()
+                f.close()
+                supportedFileType[suffix](file)
+                pass
+
+    return
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
